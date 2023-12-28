d0727e7118297bbdab34bbfed1041ebe11561cb0
nullsploit01 <harshaldharmik099@gmail.com>
2023-12-28 21:29:58 +0530

    test CI/CD

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const server = express();
 const PORT = process.env.PORT || 8080;
 
 server.get('/', (req, res) => {
-  res.json({ response: 'What do ya know? It works! Does it?' });
+  res.json({ response: 'What do ya know? It works! Does it? It Does!!' });
 });
 
 server.listen(PORT, () => {
