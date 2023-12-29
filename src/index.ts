4512b4c67950d1a0fb5858164b83c5d0265e2500
nullsploit01 <harshaldharmik099@gmail.com>
2023-12-29 13:34:44 +0530

    update scripts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 8080;
 server.set('view engine', 'ejs');
 
 server.get('/', (req, res) => {
-  //   res.json({ response: 'What do ya know? It works! Does it? It Does!!' });
   res.render('pages/index');
 });
 
