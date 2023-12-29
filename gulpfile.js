5e809a6b2e40071de16471780542bd17c1d514a8
nullsploit01 <harshaldharmik099@gmail.com>
2023-12-29 13:55:02 +0530

    add prettier

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-var { task, series, dest, src } = require('gulp');
-var { createProject } = require('gulp-typescript');
+var { task, series, dest, src } = require('gulp')
+var { createProject } = require('gulp-typescript')
 
-var tsProject = createProject('tsconfig.json');
+var tsProject = createProject('tsconfig.json')
 
 // Task which would transpile typescript to javascript
 task('typescript', function () {
-  return tsProject.src().pipe(tsProject()).js.pipe(dest('dist'));
-});
+  return tsProject.src().pipe(tsProject()).js.pipe(dest('dist'))
+})
 
 // Task which would just create a copy of the current views directory in dist directory
 task('views', function () {
-  return src('./src/views/**/*.ejs').pipe(dest('./dist/views'));
-});
+  return src('./src/views/**/*.ejs').pipe(dest('./dist/views'))
+})
 
 // Task which would just create a copy of the current static assets directory in dist directory
 task('assets', function () {
-  return src('./src/public/assets/**/*').pipe(dest('./dist/public/assets'));
-});
+  return src('./src/public/assets/**/*').pipe(dest('./dist/public/assets'))
+})
 
 // The default task which runs at start of the gulpfile.js
 task('default', series('typescript', 'views', 'assets'), () => {
-  console.log('Done');
-});
+  console.log('Done')
+})
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express from 'express';
+import express from 'express'
 
-const server = express();
-const PORT = process.env.PORT || 8080;
+const server = express()
+const PORT = process.env.PORT || 8080
 
-server.set('view engine', 'ejs');
+server.set('view engine', 'ejs')
 
 server.get('/', (req, res) => {
-  res.render('pages/index');
-});
+  res.render('pages/index')
+})
 
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+  console.log(`Server listening on port ${PORT}`)
+})
