e682f658121c8a047ec093091eed9ee29f7eec6f
nullsploit01 <harshaldharmik099@gmail.com>
2023-12-31 13:24:32 +0530

    add a file upload button

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css'
+import FileUploadButton from './components/atoms/buttons/file-upload'
+import { Box, Typography } from '@mui/material'
 
 const App = () => {
-  return <div>Catalog Digitization</div>
+  return (
+    <div>
+      <Typography variant="h4">Catalog Digitization</Typography>
+      <Box sx={{ p: 3 }}>
+        <p>Upload Product images</p>
+        <FileUploadButton />
+      </Box>
+    </div>
+  )
 }
 
 export default App
